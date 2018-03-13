7444bd159d57650b9c8017682a5ac39dd5a9a78b
Alex Mackinnon <alex@alexmackinnon.ca>
2018-03-13 16:56:09 -0400

    My first commit

---
+++ server/index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+const express = require('express');
+const  sudoku = require('sudoku-c');
+const app = express();
+
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true); //
+  next();
+});
+
+
+app.get('/suduko', function (request,response) {
+	// later expansion into a full game
+  let puzzle = {} ; //sudoku.makepuzzle();
+  let answer = sudoku.generate();
+
+  let json = {
+    'answer': answer,
+    'puzzle': puzzle
+  };
+  response.json(json);
+});
+
+app.get('/',function (request,response) {
+
+
+	response.redirect('/suduko');
+
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
---
+++ sudoku-app/src/app/app.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  private sudokuURL = 'http://localhost:3000/suduko';
+  public gameBoardRaw: any;
+
+  public answers: any;
+  public puzzle: any;
+
+  public count: number;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.getGameData();
+  }
+
+  getGameData() {
+    this.http.get(this.sudokuURL)
+      .subscribe(
+        (data: any[]) => {
+          this.gameBoardRaw = data;
+          this.answers = this.gameBoardRaw.answer;
+          this.puzzle = this.gameBoardRaw.puzzle;
+          console.log(data);
+          // console.log(this.quakes.features.length);
+        }
+      );
+
+
+  }
+
+  getChangeAnswer() {
+    this.getGameData();
+  }
+}
