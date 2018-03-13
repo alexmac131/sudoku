553fb5d386d89524d1c2f93396a0acac8c3e7315
Alex Mackinnon <alex@alexmackinnon.ca>
2018-03-13 17:25:45 -0400

    server: all 404 redirect to puzzle data css changes to reflect bootstrap 4 and not need for custom

--- server/index.js
+++ server/index.js
@@ -32,4 +32,8 @@ app.get('/',function (request,response) {
 
 });
 
+app.get('*', function(request,response){
+	response.redirect('/suduko');
+});
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
