6d62f117e9a4ec66fcae6629880e471588b2705d
Alex Mackinnon <alex@alexmackinnon.ca>
2018-03-14 17:50:47 -0400

    Remember the persons position when asking for a new board.  There is a bug at the moment where we get double selected number.  This bug is tough to reproduce and debug at the moment taking a break:

--- sudoku-app/src/app/app.component.ts
+++ sudoku-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewChecked, Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 
@@ -7,18 +7,13 @@ import {HttpClient} from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewChecked {
   private sudokuURL = 'http://localhost:3000/suduko';
   public gameBoardRaw: any;
 
   public answers: any;
   public puzzle: any;
-  public position: number = 1;
-
   public remember: number;
-
-  public count: number = 1;
-
   constructor(private http: HttpClient) {
   }
 
@@ -33,33 +28,39 @@ export class AppComponent implements OnInit{
           this.gameBoardRaw = data;
           this.answers = this.gameBoardRaw.answer;
           this.puzzle = this.gameBoardRaw.puzzle;
-          console.log(data);
-          // console.log(this.quakes.features.length);
         }
       );
-
-
   }
 
   getChangeAnswer() {
     this.getGameData();
-    this.pickSquare(this.remember);
+  }
+
+
+  rememberance() {
+    if (typeof this.remember !== 'undefined') {
+      const test = document.getElementById(this.remember.toString());
+      if (test !== null) {
+        test.classList.add('red');
+      }
+    }
   }
 
   pickSquare(value) {
-
-    console.log ('value %s remember %s', value, this.remember);
     this.remember = value;
-
-
-    let clearme = document.querySelector('.red');
+    const clearme = document.querySelector('.red');
     if (clearme !== null) {
       clearme.classList.remove('red');
     }
 
-    let test = document.getElementById(value);
-    test.classList.add('red');
-    clearme = document.querySelector('.red');
-    console.log ('clear me %s\n\n', clearme);
+    const test = document.getElementById(value);
+    if (test !== null) {
+      test.classList.add('red');
+    }
   }
+
+  ngAfterViewChecked() {
+    this.rememberance();
+  }
+
 }
